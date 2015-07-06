d39c4077cfb04be65d096e7b40f16b3ed07d1a58
kouh <kouh28@gmail.com>
2015-07-06 18:21:09 +0900

    マップデータに敵を追加

--- Assets/StageLoader.cs
+++ Assets/StageLoader.cs
@@ -4,38 +4,25 @@ using System.Collections;
 public class StageLoader : MonoBehaviour {
 
   private TextAsset textAsset;
-
-  //配置するオブジェクト
-  // public GameObject block;
-  private GameObject player;
-
-  public Vector3 createPos;
-
-  private Vector3 spaceScale = new Vector3(0.5f,0.5f,0f);
   private GameObject block;
+  private GameObject player;
+  private GameObject enemy;
+  private Vector3 spaceScale = new Vector3(0.5f,0.5f,0f);
+  
   void Start () {
-    //マップデータ取得
-    textAsset = (TextAsset)Resources.Load ("Map1");
-    // プレハブを取得
-    block = (GameObject)Resources.Load ("Block");
-    player = (GameObject)Resources.Load ("Player");
+    textAsset = (TextAsset) Resources.Load("Map1");
+    block     = (GameObject)Resources.Load("Block");
+    player    = (GameObject)Resources.Load("Player");
+    enemy     = (GameObject)Resources.Load("Enemy");
+    
     CreateStage(new Vector3(0,0,0));
-    // createPos = Vector3.zero;
-    // for (int y = 0; y < 5; y++) {
-    //   for (int x = 0; x < 5; x++) {
-    //     GameObject obj = Instantiate(block, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
-    //     obj.name = block.name;
-    //     obj.transform.parent = this.transform;
-    //   }
-    // }
   }
 
-
-  private void CreateStage(Vector3 pos){
+  private void CreateStage(Vector3 originPos){
 
     GameObject stage = new GameObject("Stage");
 
-    Vector3 originPos = pos;
+    var pos = originPos;
     string stageTextData = textAsset.text;
 
     foreach(char c in stageTextData){
@@ -51,11 +38,13 @@ public class StageLoader : MonoBehaviour {
         obj = Instantiate(player, pos, Quaternion.identity) as GameObject;
         obj.name = player.name;
         pos.x += obj.transform.lossyScale.x;
-        // obj.transform.parent = stage.transform;
-
         var camera = GameObject.Find("Camera");
         camera.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, camera.transform.position.z);
         camera.transform.parent = obj.transform;
+      }else if(c == 'e'){
+        obj = Instantiate(enemy, pos, Quaternion.identity) as GameObject;
+        obj.name = enemy.name;
+        pos.x += obj.transform.lossyScale.x;
       }else if(c == '\n'){
         pos.y -= spaceScale.y;
         pos.x = originPos.x;
